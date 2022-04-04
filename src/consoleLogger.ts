3a4e57dfb278ed4d5afb5fe333df836f6e1eb2ee
Luiz Ferraz <luiz@lferraz.com>
2022-04-04 09:00:43 -0300

    Make Logger contravariant over the log type (#17)

--- src/consoleLogger.ts
+++ src/consoleLogger.ts
@@ -1,16 +1,16 @@
-import {Log, LogDetails, Logger, LogLevel} from './logger';
+import {Log, Logger, LogLevel} from './logger';
 
 /**
  * A logger that writes to the console.
  */
-export class ConsoleLogger<T extends LogDetails = LogDetails> implements Logger<T> {
+export class ConsoleLogger<T extends Log = Log> implements Logger<T> {
     private readonly console: Console;
 
     public constructor(targetConsole: Console = console) {
         this.console = targetConsole;
     }
 
-    public log(log: Log<T>): void {
+    public log(log: T): void {
         const args = log.details === undefined
             ? [log.message]
             : [log.message, log.details];
